2f75a140d78d813766712078aa4d8a212b9afd8d
Charlie Roeder <croederdev@gmail.com>
2022-01-15 18:24:50 -0500

    added game function and called it to play 5 rounds

--- javascript.js
+++ javascript.js
@@ -49,7 +49,52 @@ function playRound(playerSelection, computerSelection){
             return "You win! Scissors beats paper!";
         }
     }
+
     else {
         return "Invalid input";
     }
 }
+
+function game(rounds){
+    let wins = 0;
+    let losses = 0;
+    let ties = 0;
+    let finalscore;
+
+    for (let i = 1; i < rounds + 1; i++){
+        let result = playRound(prompt("Round " + i), computerPlay());
+        console.log(result);
+        if (result.includes("win")){
+            wins += 1;
+        }
+        else if (result.includes("lose")){
+            losses += 1;
+        }
+        else if (result.includes("Tie")){
+            ties += 1;
+        }
+    }
+
+    if (ties === 1){
+        finalscore = wins + " to " + losses;
+    }
+    if (ties === 1){
+        finalscore = wins + " to " + losses + " with 1 tie";
+    }
+    if (ties > 1){
+        finalscore = wins + " to " + losses + " with " + ties + " ties";
+    }
+
+
+    if (wins > losses) {
+        console.log("You win the series! Final score: "+finalscore);
+    }
+    else if (wins < losses) {
+        console.log("You lose the series! Final score: "+finalscore);
+    }
+    else {
+        console.log("You tied! Final score: " + finalscore);
+    }
+}
+
+game(5);
