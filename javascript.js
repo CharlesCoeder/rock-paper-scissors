4ffad96f7637881ffd1693bdf6887393933bc3fb
Charlie Roeder <croederdev@gmail.com>
2022-01-15 17:54:49 -0500

    added playRound function

--- javascript.js
+++ javascript.js
@@ -15,3 +15,41 @@ function computerPlay(){
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   }
+
+function playRound(playerSelection, computerSelection){
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+    if (player === computer) {
+        return "Tie!";
+    }
+
+    else if (player === "rock"){
+        if (computer == "paper"){
+            return "You lose! Paper beats rock!";
+        }
+        else {
+            return "You win! Rock beats scissors!";
+        }
+    }
+
+    else if (player === "paper"){
+        if (computer == "scissors"){
+            return "You lose! Scissors beats paper!";
+        }
+        else {
+            return "You win! Paper beats rock!";
+        }
+    }
+
+    else if (player == "scissors"){
+        if (computer == "rock"){
+            return "You lose! Rock beats scissors!";
+        }
+        else {
+            return "You win! Scissors beats paper!";
+        }
+    }
+    else {
+        return "Invalid input";
+    }
+}
